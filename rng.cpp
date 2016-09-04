74c163492190fc13bc068e795bde4afbf6a7e14e
STEVETHABOSS <cyrus.boushehri@gmail.com>
2016-09-04 13:41:49 -0400

    Add files via upload

---
+++ rng.cpp
@@ -0,0 +1,14 @@
+#include "rng.h"
+#include <ctime>
+#include "mt19937ar.h"
+#include <cstring>
+
+void seedrng() {
+    init_genrand(std::time(NULL));
+}
+
+int rng(int top) {
+    if (top <= 1)
+        return 0;
+    return (int)(genrand_int32() / (0xffffffff / top + 1));
+}
